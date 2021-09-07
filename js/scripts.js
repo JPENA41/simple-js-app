69b2a79ff7158427a4448e6e4ddd408fa304df23
JPENA41 <jpenavillalba7348@gmail.com>
2021-09-07 08:33:22 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Variables //
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+// Mathematical Expressions //
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
