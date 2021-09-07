8627d56f605adc6eacc2efc8d912535daf777e3f
JPENA41 <jpenavillalba7348@gmail.com>
2021-09-07 08:40:09 -0700

    Created a variable named favoriteFood
    The variable "favoriteFood" has a value of 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
 alert('Hello world');
 
 // Variables //
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-
-// Mathematical Expressions //
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let favoriteFood = 2;
+document.write(favoriteFood);
