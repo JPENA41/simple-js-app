a364a3b7604463efa0a39a52dcb2386e0d058946
JPENA41 <jpenavillalba7348@gmail.com>
2021-09-10 09:10:19 -0700

    Indented the "ordered" list

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 let pokemonList = [
-        {
-          name: 'Bulbasaur',
-          types: ['Grass', 'Poison'],
-          height: 0.7
-        },
+  {
+    name: 'Bulbasaur',
+    types: ['Grass', 'Poison'],
+    height: 0.7
+  },
 
-        {
-          name: 'Ivysaur',
-          types: ['Grass', 'Poison'],
-          height: 1
-        },
+  {
+    name: 'Ivysaur',
+    types: ['Grass', 'Poison'],
+    height: 1
+  },
 
-        {
-          name: 'Venusaur',
-          types: ['Grass', 'Poison'],
-          height: 2
-        }
+  {
+    name: 'Venusaur',
+    types: ['Grass', 'Poison'],
+    height: 2
+  }
 ];
