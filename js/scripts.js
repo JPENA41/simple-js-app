bfaeb86f7151f2f61b0bc70108c76664ccfbcec3
JPENA41 <jpenavillalba7348@gmail.com>
2021-09-13 08:52:45 -0700

    Created a for loop that iterates over each item in pokemonList
    I added comments above my array "pokemonList" and on my for loop in "pokemonList."

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Create an array called pokemonList with objects contating the pokemon's name, height and type. //
 let pokemonList = [
   {
     name: 'Bulbasaur',
@@ -17,3 +18,27 @@ let pokemonList = [
     height: 2
   }
 ];
+
+// Created a for loop that iterates over each item in pokemonList //
+for (let i=0; i < pokemonList.length; i++){
+// Created two variable "pokemonList.name" and "pokemonlist.height" //
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+
+  if (pokemonList[i].height > 1.5){
+    document.write(
+      '<p>' +
+      pokemonName +
+      ' ' +
+      '(height: ' +
+      pokemonHeight +
+      ')' +
+      " - Wow, that's big!" +
+      '</p>'
+  );
+} else {
+  document.write(
+    '<p>' + pokemonName + ' ' + '(height: ' + pokemonHeight + ')' + '</p>'
+  );
+}
+}
